88438cc886b8324f3f8536068f0c8422b71e247f
Jess Varghese <jessvarghese@gmail.com>
2022-02-08 12:10:28 -0500

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,17 +5,17 @@ const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/idb.js',
-  '/public/css/style.css',
-  '/public/js/index.js',
-  '/public/icons/icon-72x72.png',
-  '/public/icons/icons/icon-96x96.png',
-  '/public/icons/icons/icon-128x128.png',
-  '/public/icons/icons/icon-144x144.png',
-  '/public/icons/icons/icon-152x152.png',
-  '/public/icons/icons/icon-192x192.png',
-  '/public/icons/icons/icon-384x384.png',
-  '/public/icons/icons/icon-512x512.png',
+  '/js/idb.js',
+  '/css/styles.css',
+  '/js/index.js',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 
